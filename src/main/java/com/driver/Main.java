29a93f518f2fac723500b4fbc1c55bc65938ab54
Shrayansshjain <Shrayansshjain@gmail.com>
2024-02-11 16:37:52 +0530

    override method made

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args)
+    {
+        B obj = new B();
+        String s = obj.meth();
+        System.out.print(s);
+    }
 }
