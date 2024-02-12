e0008fb9c3d869763524b48f1b511334a2a8acc0
Shrayansshjain <Shrayansshjain@gmail.com>
2024-02-12 23:21:21 +0530

    made classes in main

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,10 +2,30 @@ package com.driver;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
-        B obj = new B();
-        String s = obj.meth();
-        System.out.print(s);
+    public static class A {
+
+        public String meth() {
+            String str = "Invoking method from class A";
+            return str;
+        }
     }
+    public static class B extends A{
+
+        @Override
+        public String meth(){
+            String str = "Method is overridden in Extendend class B";
+            return str;
+        }
+    }
+        public void main(String[] args) {
+            B obj1 = new B();
+            String s = obj1.meth();
+
+            B obj2 = new B();
+            String str = obj2.meth();
+
+            System.out.println(s);
+            System.out.println(str);
+        }
+
 }
